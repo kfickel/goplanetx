e1c0bbbc2a55009cd92a0d09d5fc36165fd52139
Kaylin <kaylin.collins68@gmail.com>
2017-11-28 20:52:00 -0800

    message and response component linter

--- client/src/components/admin/message.jsx
+++ client/src/components/admin/message.jsx
@@ -1,34 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Response from './response.jsx';
+import PropTypes from 'prop-types';
+import moment from 'moment';
 import Button from 'react-bootstrap/lib/Button';
 import Checkbox from 'react-bootstrap/lib/Checkbox';
-import ControlLabel from 'react-bootstrap/lib/ControlLabel';
-import moment from 'moment';
+
+import Response from './response';
 
 class Message extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      messageId: this.props.messageId,
-      messageName: this.props.message.first_name + ' ' + this.props.message.last_name,
+      messageId: null,
+      messageName: `${props.message.first_name} ${props.message.last_name}`,
       showResponseForm: false,
-    }
+    };
+    this.onCompleteCheck = this.onCompleteCheck.bind(this);
+    this.onRespondClick = this.onRespondClick.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
     this.setState({
       messageId: nextProps.message.id,
-      messageName: nextProps.message.first_name + ' ' + nextProps.message.last_name,
+      messageName: `${nextProps.message.first_name} ${nextProps.message.last_name}`,
     });
   }
 
   // Call initiateResponse in adminView with this message's id
-  //Nice to have: indicate which response is being responded to
+  // Nice to have: indicate which response is being responded to
   onRespondClick() {
-    this.props.setResponseId(this.state.messageId);
+    // this.props.setResponseId(this.state.messageId);
     this.setState({
-      showResponseForm: !this.state.showResponseForm
+      showResponseForm: !this.state.showResponseForm,
     });
     if (this.props.admin_response === null) {
       alert('null');
@@ -39,8 +41,8 @@ class Message extends React.Component {
     this.props.setStatus(this.state.messageId);
   }
 
-  //Render all open user messages in reverse chrono order
-  //Nice to have: order by urgency
+  // Render all open user messages in reverse chrono order
+  // Nice to have: order by urgency
   render() {
     console.log('MESSAGE PROPS', this.props);
     if (this.state.showResponseForm) {
@@ -62,15 +64,30 @@ class Message extends React.Component {
             <p>{this.props.message.user_message}</p>
           </div>
           <div className="message-actions group">
-            <Checkbox onClick={this.onCompleteCheck.bind(this)} type="checkbox">Case Complete</Checkbox>
-            <Button bsStyle="primary" onClick={this.onRespondClick.bind(this)} className="admin-response-button">Hide Response Form</Button>
+            <Checkbox
+              onClick={this.onCompleteCheck}
+              type="checkbox"
+            >
+            Case Complete
+            </Checkbox>
+            <Button
+              bsStyle="primary"
+              onClick={this.onRespondClick}
+              className="admin-response-button"
+            >
+            Hide Response Form
+            </Button>
           </div>
           <div className="message-response group">
-            <Response messageName={this.state.messageName} messageId={this.state.messageId} submitAdminResponse={this.props.submitAdminResponse}/>
+            <Response
+              messageName={this.state.messageName}
+              messageId={this.state.messageId}
+              submitAdminResponse={this.props.submitAdminResponse}
+            />
           </div>
         </div>
       );
-    } else { 
+    } else {
       return (
         <div className="user-message-container group">
           <div className="message-contents group">
@@ -87,13 +104,38 @@ class Message extends React.Component {
             <span className="message-body">Your Response: <p>{this.props.message.admin_response !== null ? this.props.message.admin_response : 'You still need to response to this message'}</p></span>
           </div>
           <div className="message-actions group">
-            <Checkbox onClick={this.onCompleteCheck.bind(this)} type="checkbox">Case Complete</Checkbox>
-            <Button bsStyle="primary" onClick={this.onRespondClick.bind(this)} className="admin-response-button">Show Response Form</Button>
+            <Checkbox
+              onClick={this.onCompleteCheck}
+              type="checkbox"
+            >
+            Case Complete
+            </Checkbox>
+            <Button
+              bsStyle="primary"
+              onClick={this.onRespondClick}
+              className="admin-response-button"
+            >
+              Show Response Form
+            </Button>
           </div>
         </div>
       );
     }
   }
 }
+
+Message.propTypes = {
+  message: PropTypes.shape({
+    id: PropTypes.number,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    user_urgency: PropTypes.number,
+    user_contact: PropTypes.string,
+    user_message: PropTypes.string,
+    admin_response: PropTypes.string,
+    createdAt: PropTypes.instanceOf(Date),
+  }).isRequired,
+  submitAdminResponse: PropTypes.func.isRequired,
+};
 
 export default Message;
