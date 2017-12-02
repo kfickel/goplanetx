3d97c062a083557d035895c36323470f061f7ea1
Kaylin <kaylin.collins68@gmail.com>
2017-12-02 15:09:47 -0800

    forgot to rebase

--- client/src/components/user/tictactoeView/game.jsx
+++ client/src/components/user/tictactoeView/game.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Container, Row, Col } from 'reactstrap';
-import { Board } from './board';
 import SkyLight from 'react-skylight';
+import { Board } from './board';
 import Player from './player';
 import Computer from './computer';
 
